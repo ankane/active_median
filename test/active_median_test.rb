f49ad678b10ba8d1106975a60274349e3eb4dc42
Andrew Kane <acekane1@gmail.com>
2014-12-28 02:43:59 -0800

    Fixed test

--- test/active_median_test.rb
+++ test/active_median_test.rb
@@ -13,8 +13,8 @@ class TestActiveMedian < Minitest::Test
   end
 
   def test_odd
-    [1, 1, 2.5, 4, 100].each {|n| User.create!(visits_count: n) }
-    assert_equal 2.5, User.median(:visits_count)
+    [1, 1, 2, 4, 100].each {|n| User.create!(visits_count: n) }
+    assert_equal 2, User.median(:visits_count)
   end
 
   def test_empty
