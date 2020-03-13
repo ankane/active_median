09cdcf1aa3ccd7679e398e5e719cbe873db9ece2
Andrew Kane <andrew@chartkick.com>
2020-03-12 20:35:50 -0700

    Fixed tests

--- lib/active_median/mongoid.rb
+++ lib/active_median/mongoid.rb
@@ -9,8 +9,6 @@ module ActiveMedian
       percentile = percentile.to_f
       raise ArgumentError, "percentile is not between 0 and 1" if percentile < 0 || percentile > 1
 
-      return max(column) if percentile == 1
-
       relation =
         all
         # sort values
@@ -18,9 +16,11 @@ module ActiveMedian
         .group(_id: nil, values: {"$push" => "$#{column}"}, count: {"$sum" => 1})
         # use same logic as enumerable
         .project(values: 1, count: {"$subtract" => ["$count", 1]})
-        .project(values: 1, x: {"$multiply" => ["$count", percentile]})
-        .project(values: 1, r: {"$mod" => ["$x", 1]}, i: {"$floor" => "$x"})
-        .project(values: 1, r: 1, i: 1, i2: {"$add" => ["$i", 1]})
+        .project(values: 1, count: 1, x: {"$multiply" => ["$count", percentile]})
+        .project(values: 1, count: 1, r: {"$mod" => ["$x", 1]}, i: {"$floor" => "$x"})
+        .project(values: 1, count: 1, r: 1, i: 1, i2: {"$add" => ["$i", 1]})
+        # make sure i2 <= count (which is count(values) - 1)
+        .project(values: 1, count: 1, r: 1, i: 1, i2: {"$min" => ["$i2", "$count"]})
         .project(r: 1, beginValue: {"$arrayElemAt" => ["$values", "$i"]}, endValue: {"$arrayElemAt" => ["$values", "$i2"]})
         .project(r: 1, beginValue: 1, result: {"$subtract" => ["$endValue", "$beginValue"]})
         .project(beginValue: 1, result: {"$multiply" => ["$result", "$r"]})
