3e0421b9cc77979f7ec0cbf1daf8bc31f355fd77
Andrew Kane <andrew@chartkick.com>
2020-11-17 18:18:00 -0800

    Fixed sqlite tests [skip ci]

--- test/percentile_test.rb
+++ test/percentile_test.rb
@@ -13,6 +13,7 @@ class PercentileTest < Minitest::Test
 
   def test_odd
     skip if sqlite?
+
     [15, 20, 35, 40, 50].each { |n| User.create!(visits_count: n) }
     assert_equal 29, User.percentile(:visits_count, 0.4)
   end
@@ -28,19 +29,21 @@ class PercentileTest < Minitest::Test
   end
 
   def test_high
+    skip if sqlite?
+
     [1, 1, 2, 3, 4, 100].each { |n| User.create!(visits_count: n) }
     assert_in_delta 95.2, User.percentile(:visits_count, 0.99)
   end
 
   def test_expression
-    skip if mongoid?
+    skip if mongoid? || sqlite?
 
     [1, 2, 3, 4].each { |n| User.create!(visits_count: n) }
     assert_in_delta 4.25, User.percentile(Arel.sql("visits_count + 1"), 0.75)
   end
 
   def test_expression_no_arel
-    skip if mongoid?
+    skip if mongoid? || sqlite?
 
     message = "[active_median] Non-attribute argument: visits_count + 1. Use Arel.sql() for known-safe values. This will raise an error in ActiveMedian 0.3.0\n"
     _, stderr = capture_io do
