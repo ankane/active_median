dd3643bba2c78f0873bb21a900ba0e8b69b9c486
Andrew Kane <andrew@ankane.org>
2023-05-25 14:58:46 -0700

    Removed drop_function [skip ci]

--- lib/active_median.rb
+++ lib/active_median.rb
@@ -3,17 +3,6 @@ require "active_support"
 require "active_median/enumerable"
 require "active_median/version"
 
-module ActiveMedian
-  # TODO remove in 0.4.0
-  def self.drop_function
-    ActiveRecord::Base.connection.execute <<-SQL
-      DROP AGGREGATE IF EXISTS median(anyelement);
-      DROP FUNCTION IF EXISTS median(anyarray);
-    SQL
-    true
-  end
-end
-
 ActiveSupport.on_load(:active_record) do
   require "active_median/model"
   extend(ActiveMedian::Model)
