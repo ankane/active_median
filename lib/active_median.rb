671541f3151bf76a444e8d988a91f004f697e375
Andrew Kane <andrew@ankane.org>
2023-05-25 14:59:11 -0700

    Switched to require_relative

--- lib/active_median.rb
+++ lib/active_median.rb
@@ -1,14 +1,16 @@
+# dependencies
 require "active_support"
 
-require "active_median/enumerable"
-require "active_median/version"
+# modules
+require_relative "active_median/enumerable"
+require_relative "active_median/version"
 
 ActiveSupport.on_load(:active_record) do
-  require "active_median/model"
+  require_relative "active_median/model"
   extend(ActiveMedian::Model)
 end
 
 ActiveSupport.on_load(:mongoid) do
-  require "active_median/mongoid"
+  require_relative "active_median/mongoid"
   Mongoid::Document::ClassMethods.include(ActiveMedian::Mongoid)
 end
